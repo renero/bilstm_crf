c6946e725763180eb7eb835b2bf06ee50180fb0f
Jesus Renero <jesus.renero@gmail.com>
2018-08-16 19:42:15 +0200

    Measured accuracy

--- lstm_objetos.py
+++ lstm_objetos.py
@@ -32,6 +32,15 @@ for sentence in tqdm(datasets['utt_test']['frase'], ascii=True, file=stdout):
 
 pred = pd.DataFrame(pred)
 pred.columns = ['pred']
+
+total = hash_test.shape[0]
+hash_test = datasets['hash_test'].reset_index()
+pos = 0
+for i in range(total):
+    if pred.iloc[i][0] == hash_test.iloc[i]['tag']:
+        pos += 1
+print('Accuracy: {:.4f}'.format(pos/total))
+
 amr_pred = pred['pred'].apply(lambda x: vocabulary.construccion_amr_objetos(x))
 amr_pred = pd.DataFrame(amr_pred)
 amr_pred.columns = ['amr_pred']
